bbfdf9b5d26c1a1e34564549fa4850e3b32ba230
K-Luvin <k.luvin97@gmail.com>
2025-10-16 12:02:10 +0200

    Finished basic functionaty of the server

--- serverconnection.py
+++ serverconnection.py
@@ -1,5 +1,7 @@
 import os
 from flask import Flask, request
+from dotenv import load_dotenv
+load_dotenv()
 
 app = Flask(__name__)
 app.config['DEBUG'] = True
@@ -10,6 +12,7 @@ print("Port:", os.getenv('PORT'))
 @app.route('/')
 def index():
     print("Received a request from:", request.remote_addr)
+    return "Server is running!"
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=int(os.getenv('PORT')))
